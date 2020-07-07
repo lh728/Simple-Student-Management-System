4d349fe9f9571bbd982bc49ff8dce5ed6a7f8419
lh728 <65521533+lh728@users.noreply.github.com>
2020-07-07 16:04:32 +0800

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,55 @@
+package com.example.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    private int id;
+    private int age;
+    private String name;
+    private String sex;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString(){
+        return "id:" + id + "name:" + name + "sex:" + sex + "age:" + age;
+    }
+
+}
